1d649b252740bcec94e9aef5c64550d9bc4a3520
bee93 <bee93@LAPTOP-GU28OODI>
2023-01-07 18:27:10 +0900

    API 영화정보값 console에 찍히는 것 확인

--- src/main/webapp/js/data.js
+++ src/main/webapp/js/data.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
-	
+	$.ajax({
+		//ajax옵션 설정
+		url:"http://kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=f5eef3421c602c6cb7ea224104795888",
+		type:"GET",
+		data:{},
+		
+		//요청이 성공시 할 일 처리
+		success:function(response){
+			console.log(response)
+			console.log(typeof response)
+			//항일 처리
+			
+			//페이지단에 붙이기
+		}
+	})
 });
